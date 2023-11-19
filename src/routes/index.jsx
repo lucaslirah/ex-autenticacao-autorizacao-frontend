fd9d9459813653a0fb424d6a4d9231a34d2d2bef
LucasLiraSilva <lucasliraone@gmail.com>
2023-11-19 13:16:26 -0300

    Redirecionando Rotas de acordo com o perfil

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,29 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import { useAuth } from "../hooks/auth";
+import { USER_ROLE } from "../utils/roles";
 
+import { CustomerRoutes } from './customer.routes';
 import { AdminRoutes } from './admin.routes';
 import { AuthRoutes } from './auth.routes';
 
 export function Routes() {
   const { user } = useAuth();
 
+  function AccessRoutes(){
+    switch(user.role){
+      case USER_ROLE.ADMIN:
+        return <AdminRoutes/>
+      case USER_ROLE.CUSTOMER:
+        return <CustomerRoutes/>
+      default:
+        return <CustomerRoutes/>
+    }
+  }
+
   return (
     <BrowserRouter>
-      {user ? <AdminRoutes /> : <AuthRoutes />}
+      {user ? <AccessRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   );
 }
